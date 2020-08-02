433ed708f7300007f7bb44fe36dc7e214ad73991
Pg179TeslaaDebarpito <69044480+Pg179TeslaaDebarpito@users.noreply.github.com>
2020-08-02 09:05:28 +0530

    Add files via upload

--- review.php
+++ review.php
@@ -14,11 +14,12 @@
 		$ans4 =$_POST['ans4'];
 		$up1 =$_POST['up2'];
 		$up2 =$_POST['up1'];
+		$tot =$_POST['tot'];
 
 		$sql = "SELECT * FROM review WHERE Email = '$uemail' AND Ghat='$ghat'";
 		$result = mysqli_query($con, $sql);
 		if(mysqli_num_rows($result) > 0){
-			$sql = "UPDATE review SET ANS1='$ans1',ANS2='$ans2',ANS3='$ans3',ANS4='$ans4',Comments='$com',Name='$name',Rating='$rate',Upload1='$up1',Upload2='$up2' WHERE Email = '$uemail' AND Ghat = '$ghat'";
+			$sql = "UPDATE review SET ANS1='$ans1',ANS2='$ans2',ANS3='$ans3',ANS4='$ans4',Comments='$com',Name='$name',Rating='$rate',Upload1='$up1',Upload2='$up2',Total='$tot' WHERE Email = '$uemail' AND Ghat = '$ghat'";
 			$result = mysqli_query($con, $sql);
 			if($result)
 				echo "1";
@@ -26,7 +27,7 @@
 				echo "00";
 		}
 		elseif (mysqli_num_rows($result) <= 0 ){
-			$sql="INSERT INTO review(Name,Email,Ghat,Rating,Comments,ANS1,ANS2,ANS3,ANS4,Upload1,Upload2) VALUES ('$name','$uemail','$ghat','$rate','$com','$ans1','$ans2','$ans3','$ans4','$up1','$up2')";
+			$sql="INSERT INTO review(Name,Email,Ghat,Rating,Comments,ANS1,ANS2,ANS3,ANS4,Upload1,Upload2,Total) VALUES ('$name','$uemail','$ghat','$rate','$com','$ans1','$ans2','$ans3','$ans4','$up1','$up2','$tot')";
 			$result = mysqli_query($con,$sql);
 			if(mysqli_affected_rows($con) > 0){
 				echo "1";
